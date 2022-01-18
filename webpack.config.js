1bb2fb2ee7b82078db56b4b3674092beb7deb298
ueqri <ueqri@outlook.com>
2022-01-19 01:04:22 +0800

    Fix webpack output public path

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    // publicPath: "/",
     clean: true,
   },
   plugins: [
